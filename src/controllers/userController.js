06c86cb5588f9ffb0f4a432314e973afb2884ce0
Yero-des <yero_zc@hotmail.com>
2023-11-17 00:25:27 -0500

    New learning

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,77 +2,82 @@ const userSchema = require('../models/user');
 
 const userController = {}
 
-userController.listUser = (req, res) => {
-  userSchema
-    .find({})
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.json({ message: error});        
-    })
-};
+userController.listUser = async (req, res) => {
 
-userController.createUser = (req, res) => {
- 
-  const user = userSchema(req.body);
-
-  user
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => {
-      res.json({ message: error});
-    })
-
-};
-
-userController.findUserById = (req, res) => {
-  const { id } = req.params;
-
-  userSchema
-    .findById(id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.json({ message: error});        
-    })
-
-}
-
-userController.updateUser = (req, res) => {
-  const { id } = req.params;
-  const { name, age, email, password } = req.body;
-
-  userUpdate = {
-    name: name, 
-    age: age, 
-    email: email, 
-    password: password
+  try {
+    const data = await userSchema.find({})
+    res.json(data);
+  } catch (err) {
+    res.json({ message: err });
   }
 
-  userSchema
-    .updateOne({ _id: id }, {$set: userUpdate})
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.json({ message: error});        
-    })
+};
 
+userController.createUser = async (req, res) => {
+ 
+  try {
+    const user = userSchema(req.body);
+    await user.save();
+    res.json({ message: "Nuevo usuario agregado!" });
+  } catch (err) {
+    res.json({ message: err });
+  }
+
+};
+
+userController.findUserById = async (req, res) => {
+
+  try {
+    const { id } = req.params;
+    const data = await userSchema.findById(id);
+    res.json({
+      message: "Se encontro al usuario con exito",
+      data: data
+    });
+  } catch (err) {
+    res.json({ message: err });
+  }
 }
 
-userController.removeUser = (req, res) => {
-  const { id } = req.params;
+userController.updateUser = async (req, res) => {
 
-  userSchema
-    .deleteOne({ _id: id})
-    .then((data) => {
-      res.json(data);
+  try {
+    const { id } = req.params;
+    const { name, age, email, password } = req.body;
+    
+    const userAfterTemp = {
+      name: name, 
+      age: age, 
+      email: email, 
+      password: password
+    };
+    
+    const userBefore = await userSchema.findById(id, {} ,{ select: 'name age email password'});
+    const userAfter = await userSchema.findByIdAndUpdate(id, userAfterTemp, { new: true, select: 'name age email password'});
+
+    res.json({
+      message: "Usuario actualizado correctamente",
+      data_antigua: userBefore,
+      data_nueva: userAfter
+    });
+
+  } catch (err) {
+    res.json({ message: err });
+  }
+  
+}
+
+userController.removeUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await userSchema.deleteOne({ _id: id});
+    res.json({
+      message: `Usuario con el id ${id} ha sido eliminado`
     })
-    .catch((error) => {
-      res.json({ message: error});        
-    })
+
+  } catch (err) {
+    res.json({ message: err});
+  }
 }
 
 module.exports = userController;
